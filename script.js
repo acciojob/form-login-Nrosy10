a2d58cd8681b0a052454a271ba45643c8c620dc6
Nrosy10 <117885685+Nrosy10@users.noreply.github.com>
2023-06-10 08:56:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let form= document.getElementById("form1");
+	
+	let Fname= form.elements["fname"].value;
+	let Lname= form.elements["lname"].value;
+	alert(Fname+ " "+Lname);
 
 }
